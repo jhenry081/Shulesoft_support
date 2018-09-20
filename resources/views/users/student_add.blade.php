dacad623035b4f79b3b9f9dabaa62890721dbaaa
Joan Devops <jhenry081@gmail.com>
2018-09-20 14:48:15 +0300

    I have added divivsions in user/student module

--- resources/views/users/student_add.blade.php
+++ resources/views/users/student_add.blade.php
@@ -28,7 +28,205 @@
         </div>
     </div>
 
+<div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
 
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO CREATE CLASS LEVEL</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can create Class Level after Login and do the following steps:</p>
+
+                    <ol>
+                        <li>On Sidebar Click on Class Level</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/classlevel_add_page.png') ?>" width="80%" height="80%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>	For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/classlevel_mobile.png') ?>" width="80%" height="80%"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 
