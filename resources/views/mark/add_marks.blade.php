4e0f74b2a308853fe0baee2987e978f22991144d
Alphonce Steve <alfoncesteve@gmail.com>
2018-09-21 16:06:07 +0300

    I have done changes on Mark(add_marks)

--- resources/views/mark/add_marks.blade.php
+++ resources/views/mark/add_marks.blade.php
@@ -1,8 +1,164 @@
-<?php
+@extends('layouts.master')
+@section('content')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container-fluid">
+    <!-- .Row -->
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO ADD MARKS</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can add Marks to the subjects after Login and perform the following steps:</p>
+                    <p>Note: Before Starting the process of adding Marks, make sure that you have already define Class, Subjects and Students has restored to the system.  </p>
 
+                    <ol>
+                        <li>On the Sidebar select “Mark”</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/mark/add/1.png') ?>" width="130%" height="100%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <p>For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/2.png') ?>" width="40%" height="60%"/>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <p>Then Click Mark</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/3.png') ?>" width="40%" height="60%"/>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <ol>
+                        <li>You can Enter Marks in two ways:</li>
+                        <p> a) Enter marks Directly to the System, following steps are involved</p>
+                        <p> • Click on Add Marks</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/mark/add/4.png') ?>" width="130%" height="100%"/>
+                        </div>
+                    </ol>
+                </div>  
+                
+                  <div class="row">
+                    <p>For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/5.png') ?>" width="40%" height="60%"/>
+                    </div>
+                  </div>
+                
+                 <div class="row">
+                    <p>• Select all requirements, such as Class that you want to enter the marks in the fields as shown below</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/6.png') ?>" width="130%" height="100%"/>
+                    </div>
+                  </div>
+                
+                 <div class="row">
+                    <p>For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/7.png') ?>" width="40%" height="60%"/>
+                    </div>
+                  </div>
+                
+                 <div class="row">
+                    <p>• Then Click Mark</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/8.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/9.png') ?>" width="40%" height="60%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>• The Marks Table will be seen, click on the fields and Write the marks of the students</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/10.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                <div class="row">
+                    <p>For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/mark/add/11.png') ?>" width="40%" height="60%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>b) By Uploading Excel File to the System, Steps below are involved </p>
+                    <p>• Click on Upload Mark (Excel) </p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/12.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>• Now you can have the Excel sample format from the system by click on download icon (As shown Below), But if you have already had the required sample format you can skip this stage.</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/13.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>• The Excel sample will be seen as follows, and use that Format to prepare your Marks and save it in your documents. </p>
+                    <p>NB: While preparing Excel Sheet Enter correct Roll Number for each student that exists in the System  </p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/14.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                 <div class="row">
+                    <p>• After Saving your Excel File, Select Class, Academic Year, Semester and Exam, then click on Browse to select Excel file to the System</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/15.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                <div class="row">
+                    <p>• Choose Your File and Click Open</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/16.png') ?>" width="130%" height="100%"/>
+                    </div>
+                 </div>
+                
+                <div class="row">
+                    <p>• Then click on Upload to complete the process and to save marks to the System</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/17.png') ?>" width="130%" height="100%"/>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <p>NOTE: Successful Notification must appear when any process of adding marks is completed</p>
+                    <h4>NB:</h4>
+                    <p> Click Next to view the next list from the table</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/18.png') ?>" width="50%" height="25%"/>
+                    </div>
+                </div>
+                
+                 <div class="row">
+                    <p> Click Previous to view the list behind from the table</p>
+                    <div class="col-sm-8">
+                      <img src="<?= url('public/images/mark/add/19.png') ?>" width="50%" height="25%"/>
+                    </div>
+                </div>
+                   
+                
+                
+            </div>
+        </div>
+    </div>   
+    
+
+</div>
+
+@endsection
