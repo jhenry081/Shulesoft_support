92ab2377161cc05ad325e8c1e41b4c8f77f8fc08
Richnald charles <babarich1995@yahoo.com>
2018-09-25 14:28:14 +0300

    i have added parent view report and add cooment on report

---
+++ resources/views/exam/add_comment.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.master')
+@section('content')
+
+<div class="container-fluid">
+    <!-- .Row -->
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO WRITE COMMENT PER EACH STUDENT</h3>
+                <div class="row">
+                    <p>In ShuleSoft System you can write comment for each student registered in the System, therefore you have to Login and do the following procedures:</p>
+                    
+
+                    <ol>
+                        <li>On Sidebar Select “Exam”</li>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/comment/1.png') ?>" width="120%" height="90%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <ol>
+                        <p>• For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/comment/2.png') ?>" width="40%" height="50%"/>
+                    </div>
+</ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                    <ol>
+                    <p>	(i)Click:Exam then,<br>
+                (ii)Comment Per Student</p>
+
+
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/comment/3.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ol>
+                </div>
+                <ol>
+                <p>Click on Comment Per Student</p>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/comment/4.png') ?>" width="120%" height="90%"/>
+                    </div>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                   
+                    <ol>
+                        <p>Then Select Class Level, Class, Academic Yeah, Semester and Exam</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/comment/5.png') ?>" width="120%" height="90%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <ol>
+                    <p>• For Mobile </p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/comment/6.png') ?>" width="40%" height="50%"/>
+                    </div>
+</ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                  
+                    <ol>
+                        <p>Now you can Enter a Comment in the field by click on it, writing and click Save </p>
+                        
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/comment/7.png') ?>" width="120%" height="90%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <ol>
+                    <p>• For Mobile </p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/comment/8.png') ?>" width="40%" height="50%"/>
+                    </div>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  
+
+
+@endsection
---
+++ resources/views/exam/parent_view_report.blade.php
@@ -0,0 +1,165 @@
+@extends('layouts.master')
+@section('content')
+
+<div class="container-fluid">
+    <!-- .Row -->
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW PARENT CAN VIEW STUDENT REPORT</h3>
+                <div class="row">
+                    <p>For a parent to view a Student Report in ShuleSoft System, He/ She have to Login to the system first and follow the procedures below:</p>
+                    
+
+                    <ol>
+                        <p>1.On Sidebar Select “Exam”</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/1.png') ?>" width="130%" height="100%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                     <ol>
+                    <p>• For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/2.png') ?>" width="40%" height="60%"/>
+                    </div>
+ </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                     <ol>
+                    <p>Then Click:,<br>
+                 (i)Exam Report</p>
+
+
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/3.png') ?>" width="40%" height="60%"/>
+                        </div>
+                     </ol>
+                </div>
+                <ol>
+                <p>ii.	Select Student’s name</p>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/4.png') ?>" width="130%" height="100%"/>
+                    </div>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                   
+                    <ol>
+                        <p>• For Mobile</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/5.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                     <ol>
+                    <p> iii.	The general performance evaluation of the student will be seen, From the table click on View Report to view Exam Report</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/6.png') ?>" width="120%" height="90%"/>
+                    </div>
+ </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                  
+                    <ol>
+                        <p>• For Mobile </p>
+                        
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/7.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                     <ol>
+                    <p>•Sample report that will be displayed:</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/8.png') ?>" width="120%" height="90%"/>
+                    </div>
+                     </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                    <ul>
+                        <li>• For Mobile </li>
+
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/9.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ul>
+                </div>
+                  <div class="row">
+                     <ol>
+                    <p>iv.To view graph report, click on View Graph</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/10.png') ?>" width="120%" height="90%"/>
+                    </div>
+                     </ol>
+                </div>
+                <div class="row">
+                     <ol>
+                    <p>• For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/11.png') ?>" width="40%" height="90%"/>
+                    </div>
+ </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                    <ul>
+                        <li>•Sample graph report that will be displayed:</li>
+
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/exam/parent/12.png') ?>" width="120%" height="90%"/>
+                        </div>
+                    </ul>
+                </div>
+                <div class="row">
+                     <ol>
+                    <p> •For Mobile</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/exam/parent/13.png') ?>" width="40%" height="50%"/>
+                    </div>
+ </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+   
+
+@endsection
