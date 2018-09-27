3ceb45d8c1ded86f95a5741c27939a4e6b943778
Richnald charles <babarich1995@yahoo.com>
2018-09-27 16:39:47 +0300

    i have added content on notice

--- resources/views/notice/edit_notice.blade.php
+++ resources/views/notice/edit_notice.blade.php
@@ -1,8 +1,108 @@
-<?php
+@extends('layouts.master')
+@section('content')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container-fluid">
+    <!-- .Row -->
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <h3 class="box-title">HOW TO EDIT NOTICE IN SHULESOFT</h3>
+                <div class="row">
+                    <p>Shulesoft enables schools publish their notice. See below on steps to edit notice in ShuleSoft.</p>
+                    
 
+                    <ol>
+                        <p>1.Login into your ShuleSoft account and on sidebar click on “Notice”</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/notice/edit/1.png') ?>" width="120%" height="90%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                    <ol>
+                    <p>• For Mobile Press Menu First</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/notice/edit/2.png') ?>" width="40%" height="50%"/>
+                    </div>
+</ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                    <p>(i)Click:“Notice”</p>
+
+
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/notice/edit/3.png') ?>" width="40%" height="50%"/>
+                        </div>
+                </div>
+                <ol>
+                <p>Click on “Edit”</p>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/notice/edit/4.png') ?>" width="120%" height="90%"/>
+                    </div>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                   
+                    <ol>
+                        <p>• For Mobile</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/notice/edit/5.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ol>
+                </div>
+                <div class="row">
+                  <ol>
+                    <p> Fill the required fields and click on “Update notice”</p>
+                    <div class="col-sm-8">
+                        <img src="<?= url('public/images/notice/edit/6.png') ?>" width="120%" height="90%"/>
+                    </div>
+ </ol>
+                </div>
+                 <div class="row">
+                   
+                    <ol>
+                        <p>• For Mobile</p>
+                        <div class="col-sm-8">
+                            <img src="<?= url('public/images/notice/edit/7.png') ?>" width="40%" height="50%"/>
+                        </div>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+    
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-lg-12">
+            <div class="white-box">
+                <div class="row">
+                
+                    <ol>
+                        <p>NB: If the all procedure is done well and all text fields is full of required information,<br> the success notification will appear on top at the right corner, otherwise repeat all the procedures carefully.</p>
+                        
+                    </ol>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
