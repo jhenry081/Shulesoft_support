a074ae25cf6338d2edff76cacd840e175af56616
Ephraim Swilla <swillae1@gmail.com>
2018-10-23 15:19:29 +0300

    modify header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,29 @@
-<nav class="navbar navbar-default navbar-static-top m-b-0">
+<nav class="navbar navbar-default navbar-static-top m-b-0" >
             <div class="navbar-header">
                 <div class="top-left-part">
                     <!-- Logo -->
-                    <a class="logo">
+                    <a class="logo" href="<?=url('/')?>">
                         <!-- Logo icon image, you can use font-icon also --><b>
-                        <!--This is dark logo icon--><img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="100%" alt="home" class="dark-logo" />
-                        <!--This is light logo icon--><img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="100%" alt="home" class="light-logo" />
+                        <!--This is dark logo icon-->
+                        <img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="150" alt="home" class="dark-logo" />
+                        <!--This is light logo icon-->
+                        <img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="150" alt="home" class="light-logo" />
                      </b>
                         <!-- Logo text image you can use text also --><span class="hidden-xs">
                         <!--This is dark logo text-->
-                        <img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="100%" alt="home" class="dark-logo" />
+                        <img src="<?= url('public/images/ShuleSoft-TM.png') ?>" width="150" alt="home" class="dark-logo" />
                         <!--This is light logo text-->
                      </span> </a>
                 </div>
                 <!-- /Logo -->
                 <!-- Search input and Toggle icon -->
-                <ul class="nav navbar-top-links navbar-left">
+                <ul class="nav navbar-top-links navbar-left" >
                     <li><a href="javascript:void(0)" class="open-close waves-effect waves-light"><i class="ti-menu"></i></a></li>
                     <li class="dropdown">
-                        <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"> <i class="mdi mdi-gmail"></i>
+<!--                        <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"> <i class="mdi mdi-gmail"></i>
                             <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
-                        </a>
-                        <ul class="dropdown-menu mailbox animated bounceInDown">
+                        </a>-->
+<!--                        <ul class="dropdown-menu mailbox animated bounceInDown">
                             <li>
                                 <div class="drop-title">You have 4 new messages</div>
                             </li>
@@ -52,15 +54,16 @@
                             <li>
                                 <a class="text-center" href="javascript:void(0);"> <strong>See all notifications</strong> <i class="fa fa-angle-right"></i> </a>
                             </li>
-                        </ul>
+                        </ul>-->
                         <!-- /.dropdown-messages -->
                     </li>
                     <!-- .Task dropdown -->
                     <li class="dropdown">
-                        <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"> <i class="mdi mdi-check-circle"></i>
+<!--                        <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"> 
+                            <i class="mdi mdi-check-circle"></i>
                             <div class="notify"><span class="heartbit"></span><span class="point"></span></div>
-                        </a>
-                        <ul class="dropdown-menu dropdown-tasks animated slideInUp">
+                        </a>-->
+<!--                        <ul class="dropdown-menu dropdown-tasks animated slideInUp">
                             <li>
                                 <a href="#">
                                     <div>
@@ -108,10 +111,10 @@
                             <li>
                                 <a class="text-center" href="#"> <strong>See All Tasks</strong> <i class="fa fa-angle-right"></i> </a>
                             </li>
-                        </ul>
+                        </ul>-->
                     </li>
                     <!-- .Megamenu -->
-                    <li class="mega-dropdown"> <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"><span class="hidden-xs">Mega</span> <i class="icon-options-vertical"></i></a>
+<!--                    <li class="mega-dropdown"> <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"><span class="hidden-xs">Mega</span> <i class="icon-options-vertical"></i></a>
                         <ul class="dropdown-menu mega-dropdown-menu animated bounceInDown">
                             <li class="col-sm-3">
                                 <ul>
@@ -167,15 +170,16 @@
                             </li>
                             
                         </ul>
-                    </li>
+                    </li>-->
                     <!-- /.Megamenu -->
                 </ul>
-                <ul class="nav navbar-top-links navbar-right pull-right">
-                    <li>
+                <ul class="nav navbar-top-links navbar-right pull-right" style="background: #efefef !important;
+    border-bottom: 1px solid #eee !important;">
+<!--                    <li>
                         <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
                             <input type="text" placeholder="Search..." class="form-control"> <a href="#"><i class="fa fa-search"></i></a> </form>
-                    </li>
-                    <li class="dropdown">
+                    </li>-->
+<!--                    <li class="dropdown">
                         <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="../plugins/images/users/varun.jpg" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">Steave</b><span class="caret"></span> </a>
                         <ul class="dropdown-menu dropdown-user animated flipInY">
                             <li>
@@ -193,8 +197,8 @@
                             <li role="separator" class="divider"></li>
                             <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
                         </ul>
-                        <!-- /.dropdown-user -->
-                    </li>
+                         /.dropdown-user 
+                    </li>-->
                     
                     <!-- /.dropdown -->
                 </ul>
